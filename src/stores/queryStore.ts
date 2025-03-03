fa658d808e88cce3a4e9ccc71abc5a645aef5569
Alun Jones <alunacjones@gmail.com>
2025-03-03 16:02:49 +0000

    don't use query params

--- src/stores/queryStore.ts
+++ src/stores/queryStore.ts
@@ -1,10 +1,10 @@
-import { useStorage, useUrlSearchParams } from "@vueuse/core";
+import { useStorage } from "@vueuse/core";
 import moment from "moment";
 import { defineStore } from "pinia";
 import { type Ref } from "vue";
 import { type Region, type Platform } from "../utils/api";
 
-interface ISearchParams
+export interface ISearchParams
 {
     searchString: string
     genre: string
@@ -18,20 +18,17 @@ interface ISearchParams
 }
 
 export const useQueryStore = defineStore("query", {
-    state() {
-        return useUrlSearchParams<ISearchParams>("history", {
-            initialValue: {
-                platform: useStorage<Platform>("platform", "popular"),
-                region: useStorage<Region>("region", "yorkshire"),
-                searchString: "",
-                genre: "",
-                type: "",
-                hideEmpty: useStorage<boolean>("hideEmpty", false),
-                day: moment(new Date()).format("yyyy-MM-DD"),
-                category: "",
-                liveOnly: false
-            }            
-        });
-    },
-
+    state(): ISearchParams {
+        return {
+            platform: useStorage<Platform>("platform", "popular"),
+            region: useStorage<Region>("region", "yorkshire"),
+            searchString: "",
+            genre: "",
+            type: "",
+            hideEmpty: useStorage<boolean>("hideEmpty", false),
+            day: moment(new Date()).format("yyyy-MM-DD"),
+            category: "",
+            liveOnly: false         
+        };
+    }
 })
